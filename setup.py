4d9e35aa8f42ab02317a10a76e9a3ad95eb6c780
lkot <konstantinliuterovich@gmail.com>
2017-10-27 23:31:18 +0700

    implemented project file structure

---
+++ setup.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+from setuptools.command.test import test as TestCommand
+import sys
+from setuptools import setup
+
+__version__ = "0.0.1"
+
+
+class Tox(TestCommand):
+    user_options = [('tox-args=', 'a', "Arguments to pass to tox")]
+    tox_args = None
+    test_args = None
+    test_suite = None
+
+    def initialize_options(self):
+        TestCommand.initialize_options(self)
+        self.tox_args = None
+
+    def finalize_options(self):
+        TestCommand.finalize_options(self)
+        self.test_args = []
+        self.test_suite = True
+
+    def run_tests(self):
+        # import here, cause outside the eggs aren't loaded
+        import tox
+        import shlex
+        args = self.tox_args
+        if args:
+            args = shlex.split(self.tox_args)
+        errno = tox.cmdline(args=args)
+        sys.exit(errno)
+
+
+setup(
+    name='django-data-models',
+    version=__version__,
+    packages=['drf_swagger_yaml'],
+    include_package_data=True,
+    url='https://github.com/TriplePoint-Software/drf-swagger-yaml',
+    license='Apache License, Version 2.0',
+    author='Konstantin Liuterovich',
+    author_email='konstantinliuterovich@gmail.com',
+    description='Django\'s Drf swagger yaml',
+    install_requires=["django>=1.8,<1.11", "psycopg2>=2.6.2", 'pyyaml'],
+    tests_require=["tox", "pyyaml"],
+    cmdclass={'test': Tox},
+)
---
+++ tests/settings.py
@@ -0,0 +1,44 @@
+"""
+Django settings for _test_project project.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.7/topics/settings/
+
+For the full list of settings and their values, see
+https://docs.djangoproject.com/en/1.7/ref/settings/
+"""
+# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+import os
+BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'test_key'
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+TEMPLATE_DEBUG = True
+
+
+# Application definition
+
+INSTALLED_APPS = (
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'drf_swagger_yaml',
+    'tests',
+)
+
+# Database
+# https://docs.djangoproject.com/en/1.7/ref/settings/#databases
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': 'tests.sqlite3',
+    },
+}
+
